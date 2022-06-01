c166936fa6cfbd43aec8b9b059e770d54a4d0a83
Teddy-FN <tedd.ferdy@gmail.com>
2022-06-01 11:20:42 +0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,31 @@
+import PokeLogo from "./img/pokeball.png";
+import PokemonPage from "./img/pokemonHeader.jpg";
+
+import { RiHome3Line } from "react-icons/ri";
+import { RiStore3Line } from "react-icons/ri";
+import { RiCalendarEventLine } from "react-icons/ri";
+import { RiMapPinUserLine } from "react-icons/ri";
+import { RiPagesLine } from "react-icons/ri";
+import { RiSearchEyeLine } from "react-icons/ri";
+import { RiShoppingCart2Fill } from "react-icons/ri";
+import { RiNotification3Line } from "react-icons/ri";
+import { RiQuestionnaireLine } from "react-icons/ri";
+import { RiHeartLine } from "react-icons/ri";
+
+export const Icons = {
+  RiHome3Line,
+  RiStore3Line,
+  RiCalendarEventLine,
+  RiMapPinUserLine,
+  RiPagesLine,
+  RiSearchEyeLine,
+  RiShoppingCart2Fill,
+  RiNotification3Line,
+  RiQuestionnaireLine,
+  RiHeartLine,
+};
+
+export const Images = {
+  PokeLogo,
+  PokemonPage,
+};
---
+++ src/components/Pages/Main/MainMenu.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Icons, Images } from "../../../assets";
+import "./mainMenu.css";
+
+const MainMenu = () => {
+  console.log(Icons);
+  return (
+    <div className="layout">
+      <nav>
+        <ul className="main-features">
+          <li className="main-logo">
+            <img src={Images.PokeLogo} alt="pokeBall" className="img-logo" />
+          </li>
+          <li className="main-items">
+            <a href="#">
+              <Icons.RiHome3Line className="main-icons" />
+            </a>
+          </li>
+          <li className="main-items">
+            <a href="#">
+              <Icons.RiPagesLine className="main-icons" />
+            </a>
+          </li>
+          <li className="main-items">
+            <a href="#">
+              <Icons.RiCalendarEventLine className="main-icons" />
+            </a>
+          </li>
+          <li className="main-items">
+            <a href="#">
+              <Icons.RiStore3Line className="main-icons" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <menu className="menu">
+        <div className="inputSearch">
+          <input type="text" placeholder="Search" className="input" />
+          <Icons.RiSearchEyeLine className="iconsSeacrh" />
+        </div>
+        <div className="items user-features">
+          <a href="#" className="cart">
+            <Icons.RiHeartLine />
+            Wish List
+          </a>
+          <a href="#" className="cart">
+            <Icons.RiQuestionnaireLine />
+            Support
+          </a>
+          <a href="#" className="cart">
+            <Icons.RiShoppingCart2Fill />
+            Cart
+          </a>
+          <a href="#" className="notifcation">
+            <Icons.RiNotification3Line />
+          </a>
+          <a href="#" className="login">
+            <Icons.RiMapPinUserLine />
+            Login / Sign up
+          </a>
+        </div>
+      </menu>
+      <aside className="aside">
+        <div className="img-header">
+          <img
+            src={Images.PokemonPage}
+            alt="pokemon logo"
+            className="main-img"
+          />
+          <div>
+            <h2>Get Ready For The Adventure With Pikacu ⚡️ !</h2>
+            <p></p>
+          </div>
+        </div>
+        <div className="container grid two-cols">
+          <div>Content1</div>
+          <div>Content2</div>
+        </div>
+      </aside>
+    </div>
+  );
+};
+
+export default MainMenu;
