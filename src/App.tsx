d2dac6999b199a449f9b7372eedc66862dfe1d9a
Teddy-FN <tedd.ferdy@gmail.com>
2024-04-23 15:04:36 +0700

    Fixing Ts Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 // PAGES
 import "./components/Pages/Main/mainMenu.css";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import Login from "./components/Pages/Login";
-import Home from "./components/Pages/Home";
-import Store from "./components/Pages/Store";
-import MainMenu from "./components/Pages/Main/MainMenu";
-import PokemonPages from "./components/Pages/PokemonPages";
+import Login from "@/components/Pages/Login";
+import Home from "@/components/Pages/Home";
+import Store from "@/components/Pages/Store";
+import MainMenu from "@/components/Pages/Main";
+import PokemonPages from "@/components/Pages/PokemonPages";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} exact />
-        <Route path="/" element={<Home />} exact>
-          <Route path="/" element={<MainMenu />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/pokemon" element={<PokemonPages />} />
-        </Route>
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />}>
+            <Route path="/" element={<MainMenu />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/pokemon" element={<PokemonPages />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
