625d79138aca1045e3af39475fc44b298f7b3461
Teddy-FN <tedd.ferdy@gmail.com>
2024-04-24 14:14:43 +0700

    Fixing Setup Project Error

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import React from "react";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 // PAGES
 import "./components/Pages/Main/mainMenu.css";
-import Login from "@/components/Pages/Login";
-import Home from "@/components/Pages/Home";
-import Store from "@/components/Pages/Store";
-import MainMenu from "@/components/Pages/Main";
-import PokemonPages from "@/components/Pages/PokemonPages";
+// import Login from "@/components/Pages/Login";
+import Home from "./components/Pages/Home";
+// import Store from "@/components/Pages/Store";
+// import MainMenu from "@/components/Pages/Main";
+// import PokemonPages from "@/components/Pages/PokemonPages";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          {/* <Route path="/login" element={<Login />} /> */}
           <Route path="/" element={<Home />}>
-            <Route path="/" element={<MainMenu />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/pokemon" element={<PokemonPages />} />
+            {/* <Route path="/" element={<MainMenu />} /> */}
+            {/* <Route path="/store" element={<Store />} /> */}
+            {/* <Route path="/pokemon" element={<PokemonPages />} /> */}
           </Route>
         </Routes>
       </Router>
