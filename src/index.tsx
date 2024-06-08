6fe43f93125bc6bc9dac3e8d591784eed1296afb
Teddy-FN <tedd.ferdy@gmail.com>
2024-06-08 14:09:08 +0700

    Deleting Folder Not Used In Project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import { Provider } from "react-redux";
-// import { store } from "./redux";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
+  document.getElementById("root"),
 );
