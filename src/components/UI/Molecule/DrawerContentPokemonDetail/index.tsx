67694984f564c6b6af6535df2cc6031c6a1ff6bf
Teddy-FN <tedd.ferdy@gmail.com>
2024-06-17 22:43:37 +0700

    Add Stats In Pokemon Detail

--- src/components/UI/Molecule/DrawerContentPokemonDetail/index.tsx
+++ src/components/UI/Molecule/DrawerContentPokemonDetail/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 import React, { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
